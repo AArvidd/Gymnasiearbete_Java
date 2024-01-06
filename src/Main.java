398b0d701a7bcd204c37f7db4f5a6f099dcba827
AArvidd <arvid.oscarsson@elev.ga.ntig.se>
2024-01-06 19:58:14 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,24 @@ public class Main {
         float start = System.nanoTime();
 
         ArrayList<Sphere> sean = new ArrayList<Sphere>();
-        sean.add(new Sphere(50, 0, 10, 40, 0.5f, 255, 0, 255));
-        sean.add(new Sphere(50, -200, 10, 40, 0, 0, 0, 255));
+        sean.add(new Sphere(30, 0, 0, 10, 0.5f, 255, 0, 255));
+        sean.add(new Sphere(20, -20, 0, 5, 0.25f, 0, 0, 255));
+        sean.add(new Sphere(50, 0, 20, 10, 0.5f, 255, 0, 0));
+        sean.add(new Sphere(100, 100, 10, 40, 1, 255, 255, 255));
+        sean.add(new Sphere(10, 0, 10, 5, 0.5f, 0, 255, 0));
+        sean.add(new Sphere(-10, 0, 0, 5, 0, 255, 255, 255));
+        sean.add(new Sphere(0, 20, 0, 5, 0, 0, 255, 255));
+        sean.add(new Sphere(100, 100, 100, 10, 0.1f, 255, 255, 0));
+        sean.add(new Sphere(-100, -100, 0, 10, 0, 0, 0, 255));
+        sean.add(new Sphere(100, -100, 100, 10, 0, 128, 128, 128));
 
 
-        Ground ground = new Ground(-20, 0.5f, new int[]{0, 0, 0}, new int[]{255, 255, 255});
+        Ground ground = new Ground(-20, 0.25f, new int[]{0, 0, 0}, new int[]{255, 255, 255});
 
-        Camera main = new Camera(0, 0, -19, 3000, 3000, 50, sean, ground, 0, 0, 0);
+        Camera main = new Camera(0, 0, 0, 5000, 5000, 2, sean, ground, 0, 0, 0);
 
         BufferedImage image = main.makeImage();
-        File output = new File("output.png");
+        File output = new File("outputJava.png");
         ImageIO.write(image, "png", output);
 
 
@@ -26,7 +34,7 @@ public class Main {
 
         float dif = end - start;
 
-        System.out.println(dif);
+        System.out.println(dif / 1000000000);
 
 
     }
